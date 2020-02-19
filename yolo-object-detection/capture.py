3557a912da0d176632a40ce02023f1189a793d5e
henrytong <henryyy040606@hotmail.com>
2020-02-19 15:09:35 +0800

    update web by henry

--- yolo-object-detection/capture.py
+++ yolo-object-detection/capture.py
@@ -1,8 +1,11 @@
 import socket
 
-# socket for RPi connection
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.settimeout(5)
 client.connect(('192.168.137.10', 65432))
 
-if bool(client.recv(1024).decode()): 
-	client.close()
+
+flag = bool(client.recv(1024).decode())
+
+if flag:
+    client.close()
